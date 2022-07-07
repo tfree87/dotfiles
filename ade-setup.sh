ee0aeb45eddeb96c59cc7bd5e9c9e01cad97dcc5
tfree76 <tlfree87@gmail.com>
2022-07-07 15:48:09 -0400

    Use Org Mode to tangle different startup script files

--- ade-setup.sh
+++ ade-setup.sh
@@ -1,3 +1,4 @@
+!/bin/sh
 # Install Adobe Digital Editions using WINE
 
 export WINEPREFIX="~/.wine32"
@@ -5,8 +6,3 @@ export WINEPREFIX="~/.wine32"
 WINEARCH=win32
 winecfg
 winetricks -q adobe_diged4
-
-# Install extensions for Gnome
-
-
-
