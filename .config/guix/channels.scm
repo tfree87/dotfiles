24f821b1c9f78647fffd4d800f39ae3c6e0d06a8
tfree76 <tlfree87@gmail.com>
2022-07-25 19:19:34 -0400

    Update channels.sc with current commits

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,33 +1,33 @@
 (list (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
-       (branch "master")
-       (commit
-          "75a520cca62ea105a6fbb7f1d74b3a4ef439cd73")
-       (introduction
-        (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
-         (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (branch "master")
+        (commit
+          "657da22f0229b978b7bf4e4d476f59f17f6a175f")
+        (introduction
+          (make-channel-introduction
+            "33f86a4b48205c0dc19d7c036c85393f0766f806"
+            (openpgp-fingerprint
+              "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
       (channel
-       (name 'nonguix)
-       (url "https://gitlab.com/nonguix/nonguix")
-       (branch "master")
-       (commit
-          "e84289b07631202caeabd4496a2f9db5293c6b9f")
-       (introduction
-        (make-channel-introduction
-         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-         (openpgp-fingerprint
-          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "674d04a5fbd8689ab5ff27271a656f711fc77c54")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-       (name 'flat)
-       (url "https://github.com/flatwhatson/guix-channel.git")
-       (branch "master")
-       (commit
-        "657da22f0229b978b7bf4e4d476f59f17f6a175f")
-       (introduction
-        (make-channel-introduction
-         "33f86a4b48205c0dc19d7c036c85393f0766f806"
-         (openpgp-fingerprint
-          "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490")))))       
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "cda3de3b7de46346457e75eb7022bc76a34c2d4f")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
